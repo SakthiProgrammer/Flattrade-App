0c0893a8d93924324e6b17221559bc8dca03647e
Sakthiprogrammer <sakthiveld479@gmail.com>
2025-01-19 17:27:08 +0530

    feat:edit bank added in the table for admin usage

--- Vue/frontend/src/components/admin/bank/components/BankTable.vue
+++ Vue/frontend/src/components/admin/bank/components/BankTable.vue
@@ -3,18 +3,31 @@
     <v-card>
       <div class="d-flex justify-space-between align-center">
         <v-card-title class="text-h5">Bank Details</v-card-title>
-        <v-btn color="primary" class="mr-5" @click="dialog = true">Add Bank</v-btn>
+        <v-btn color="primary" class="mr-5" @click="dialog = true"
+          >Add Bank</v-btn
+        >
       </div>
       <v-card-text>
-        <v-data-table :headers="headers" :items="banks" class="elevation-1" fixed-header>
+        <v-data-table
+          :headers="headers"
+          :items="banks"
+          class="elevation-1"
+          fixed-header
+        >
           <template v-slot:body="{ items }">
             <tbody>
               <tr v-for="(bank, index) in items" :key="bank.id">
-                <td>{{ index + 1 }}</td> <!-- S.No -->
+                <td>{{ index + 1 }}</td>
+                <!-- S.No -->
                 <td>{{ bank.bank_name }}</td>
                 <td>{{ bank.branch_name }}</td>
                 <td>{{ bank.ifsc_code }}</td>
                 <td>{{ bank.address }}</td>
+                <td>
+                  <v-btn color="red" small @click="editBank(bank)">
+                    <v-icon>mdi-pencil</v-icon>
+                  </v-btn>
+                </td>
               </tr>
             </tbody>
           </template>
@@ -28,10 +41,26 @@
           <v-card-title class="text-h5">Upload Bank Details</v-card-title>
           <v-form>
             <v-card-text>
-              <v-text-field label="Bank Name" v-model="newBank.bank_name" required></v-text-field>
-              <v-text-field label="Branch Name" v-model="newBank.branch_name" required></v-text-field>
-              <v-text-field label="IFSC Code" v-model="newBank.ifsc_code" required></v-text-field>
-              <v-text-field label="Address" v-model="newBank.address" required></v-text-field>
+              <v-text-field
+                label="Bank Name"
+                v-model="newBank.bank_name"
+                required
+              ></v-text-field>
+              <v-text-field
+                label="Branch Name"
+                v-model="newBank.branch_name"
+                required
+              ></v-text-field>
+              <v-text-field
+                label="IFSC Code"
+                v-model="newBank.ifsc_code"
+                required
+              ></v-text-field>
+              <v-text-field
+                label="Address"
+                v-model="newBank.address"
+                required
+              ></v-text-field>
             </v-card-text>
             <v-card-actions>
               <v-spacer></v-spacer>
@@ -46,61 +75,77 @@
 </template>
 
 <script>
-import EventService from '../../../../Services/EventService';
+import EventService from "../../../../Services/EventService";
 
 export default {
   data() {
     return {
       dialog: false,
       newBank: {
-        bank_name: '',
-        branch_name: '',
-        ifsc_code: '',
-        address: '',
+        bank_name: "",
+        branch_name: "",
+        ifsc_code: "",
+        address: "",
       },
       headers: [
-        { text: 'S.No', value: 'sno', sortable: false },
-        { text: 'Bank Name', value: 'bank_name' },
-        { text: 'Branch Name', value: 'branch_name' },
-        { text: 'IFSC Code', value: 'ifsc_code' },
-        { text: 'Address', value: 'address' },
+        { text: "S.No", value: "sno", sortable: false },
+        { text: "Bank Name", value: "bank_name" },
+        { text: "Branch Name", value: "branch_name" },
+        { text: "IFSC Code", value: "ifsc_code" },
+        { text: "Address", value: "address" },
+        { text: "Edit", value: "edit" },
       ],
-      banks: [],
+      banks: [{ bank_name: "UNION", branch_name: "Pudupalayam" }],
     };
   },
   methods: {
     submitBankDetails() {
       this.banks.push({ ...this.newBank, id: Date.now() });
       this.dialog = false;
-      this.newBank = { bankName: '', branchName: '', ifscCode: '', address: '' };
+      this.newBank = {
+        bankName: "",
+        branchName: "",
+        ifscCode: "",
+        address: "",
+      };
     },
     addBank() {
       EventService.CreateBank(this.newBank)
         .then((res) => {
           if (res.data.status == "S") {
-            console.log("Bank Created")
-            this.banks.push(this.newBank)
+            console.log("Bank Created");
+            this.banks.push(this.newBank);
             this.dialog = false;
-            this.newBank = { bankName: '', branchName: '', ifscCode: '', address: '' };
-
+            this.newBank = {
+              bankName: "",
+              branchName: "",
+              ifscCode: "",
+              address: "",
+            };
           } else {
-            console.log(
-              res.data.errMsg
-            )
+            console.log(res.data.errMsg);
           }
-        }).catch((err) => console.log(err))
-
-    }
+        })
+        .catch((err) => console.log(err));
+    },
+    editBank(bank) {
+      this.newBank.bank_name = bank.bank_name;
+      this.newBank.branch_name = bank.branch_name;
+      this.newBank.address = bank.address;
+      this.newBank.ifsc_code = bank.ifsc_code;
+      this.dialog = true;
+    },
   },
   beforeMount() {
     EventService.GetBankDetails("/getbanks")
       .then((res) => {
         if (res.data.status == "S") {
-          this.banks = res.data.bank_details
+          this.banks = res.data.bank_details;
         } else {
           console.log("err", res.data.errMsg);
         }
-      }).catch((err) => console.log(err))
-  }
+      })
+      .catch((err) => console.log(err));
+  },
 };
 </script>
